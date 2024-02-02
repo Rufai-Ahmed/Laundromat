a1c9f2b74fea07ff248d42b207125af9f473275e
chinwe-regina <chiorichinwe21@gmail.com>
2024-02-02 14:35:24 +0100

    another update on router

--- BE/router/serviceProvider.ts
+++ BE/router/serviceProvider.ts
@@ -1,5 +1,5 @@
 import {Router} from "express"
-import { CreateServiceprovider, LoginServiceProvider, ReadallServiceProvider, ReadoneServiceProvider, UpdateserviceProvidername, VerifyServiceprovider } from "../controller/Serviceprocontroller";
+import { CreateServiceprovider, LoginServiceProvider, ReadallServiceProvider, ReadoneServiceProvider, VerifyServiceprovider } from "../controller/Serviceprocontroller";
 
 const router:Router = Router()
 router.route("/register-Serviceprovider").post(CreateServiceprovider );
@@ -7,7 +7,7 @@ router.route("/verify-Serviceprovider").patch(VerifyServiceprovider  );
 router.route("/login-Serviceprovider").post(LoginServiceProvider  );
 router.route("/view-one-Serviceprovider/:ServProID").get(ReadoneServiceProvider);
 router.route("/view-all-Serviceprovider").get(ReadallServiceProvider);
-router.route("/update-Serviceprovider").get(UpdateserviceProvidername);
+
 
 export default router;
 
