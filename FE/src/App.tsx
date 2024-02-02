006e812ce32ad9dd2a576024448b4fc665eff082
Rufai-Ahmed <abbeyrufai234@gmail.com>
2024-02-02 12:41:34 +0100

    first push

---
+++ FE/src/App.tsx
@@ -0,0 +1,14 @@
+import TestQuestion from "./TestQuestion";
+
+document.title = "Homescreen";
+
+function App() {
+  return (
+    <div>
+      <TestQuestion />
+      {/* <Test /> */}
+    </div>
+  );
+}
+
+export default App;
