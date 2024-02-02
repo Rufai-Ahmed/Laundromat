60ef0c23ba8615d9a7eecb05de5b4dc15022ef7b
Peters Wisdom <peterswisdom603@gmail.com>
2024-02-02 17:54:47 +0100

    service

--- BE/mainApp.ts
+++ BE/mainApp.ts
@@ -1,10 +1,12 @@
 import { Application, Request, Response } from "express";
 import customer from "./router/customerRouter";
-import service from "./router/serviceProvider";
+import servicepro from "./router/serviceProvider";
+import service from "./router/Servicerouter";
 
 export const Mainapp = (app: Application) => {
-  app.use("/api", service);
+  app.use("/api", servicepro);
   app.use("/api", customer);
+  app.use("/api", service);
 
   app.get("/", (req: Request, res: Response) => {
     try {
