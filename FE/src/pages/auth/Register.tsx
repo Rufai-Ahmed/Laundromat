314803456a023cfdb7d21f8be6cb4bdb84bf2aa2
Rufai-Ahmed <abbeyrufai234@gmail.com>
2024-02-02 17:48:01 +0100

    Added auth to frontend

--- FE/src/pages/auth/Register.tsx
+++ FE/src/pages/auth/Register.tsx
@@ -2,38 +2,24 @@ import { NavLink, useLocation } from "react-router-dom";
 import dummy from "../../Assets/dummy.jpg";
 import logo from "../../Assets/logoname.png";
 import { useState } from "react";
+import Client from "./User";
 
 export const Register = () => {
   const { pathname } = useLocation();
-  const [click, setClick] = useState(false);
+
+  console.log(pathname);
 
   return (
-    <div className="">
-      <center className="absolute top-5 w-full justify-center">
-        <img src={logo} className="scale-75" />
+    <div>
+      <center className="">
+        <img src={logo} className="scale-75 " />
       </center>
       {pathname.includes("/client") ? (
-        <div className="w-full h-screen flex justify-center items-center">
-          <div className="w-full flex items-center flex-col">
-            <div
-              className="min-w-[310px] relative"
-              onClick={() => setClick(true)}
-            >
-              <label
-                className={`absolute left-3 ${click ? "-top-7" : "top-3"}`}
-              >
-                Email
-              </label>
-              <input
-                type="text"
-                placeholder=""
-                className="input input-bordered bg-white input-info w-full max-w-xs"
-              />
-            </div>
-          </div>
-        </div>
+        <Client client="" />
+      ) : pathname.includes("/provider") ? (
+        <Client provider="" />
       ) : (
-        <div className="w-full flex items-center justify-center h-screen gap-7 flex-wrap py-40 md:py-0">
+        <div className="w-full flex items-center justify-center gap-7 flex-wrap py-40 md:py-0">
           <NavLink
             to="client"
             className="card card-bordered border-blue-600 border-[4px] cursor-pointer transition-all duration-300 hover:shadow-xl w-[280px] hover:scale-105 min-h-[400px]"
---
+++ FE/src/pages/auth/User.tsx
@@ -0,0 +1,78 @@
+import { FC, useState } from "react";
+import { NavLink } from "react-router-dom";
+
+interface iUser {
+  client?: string;
+  provider?: string;
+}
+
+const User: FC<iUser> = ({ client, provider }) => {
+  const [click, setClick] = useState(false);
+  const [click1, setClick1] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <div>
+      {" "}
+      <div className="w-full flex justify-center ">
+        <div className="w-[400px] flex items-center space-y-10 flex-col">
+          <div
+            className="relative w-full"
+            onClick={() => setClick(true)}
+            onBlur={() => setClick(email ? true : false)}
+          >
+            <label
+              className={`absolute transition-all duration-300 ${
+                click ? "-top-7" : "top-3 left-3"
+              }`}
+            >
+              Email
+            </label>
+            <input
+              type="text"
+              placeholder=""
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+              className="input input-bordered bg-white input-info w-full "
+            />
+          </div>
+          <div className=" relative w-full">
+            <label
+              className={`absolute transition-all duration-300 ${
+                click1 ? "-top-7" : "top-3 left-3"
+              }`}
+            >
+              Password
+            </label>
+            <input
+              type="text"
+              placeholder=""
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              onBlur={() => {
+                setClick1(password ? true : false);
+              }}
+              onClick={() => {
+                setClick1(true);
+              }}
+              className="input input-bordered bg-white input-info w-full "
+            />
+            <button className="btn btn-primary text-white text-[17px] bg-blue-600 mt-10 w-full">
+              Register
+            </button>
+
+            <center className="mt-3">
+              Have an account?{" "}
+              <NavLink className="text-blue-600" to="/auth/login/client">
+                Log in here
+              </NavLink>
+            </center>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default User;
